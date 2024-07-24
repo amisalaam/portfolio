5c0378cca4cf2c81a0c3bc5089459acc56f9178d
amisalaam <amisalaam9@gmail.com>
2024-07-24 21:52:42 +0530

    changed bg color

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 function App() {
   return (
-    <div>
+    <div className='bg-black text-white '>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
